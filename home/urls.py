eb871dc98f2f4c8a9b8d113df24001ef650bb480
sbshchen <shikun.chen@stud.uni-due.de>
2021-07-02 10:08:55 +0200

    add another pages

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.template import loader
+from django.http import HttpResponse
+from django import template
+
 
 @login_required(login_url="authentication/login/")
 def home(request):
     context = {}
     context['segment'] = 'index'
     return render(request, 'index.html', context)
+
+
+@login_required(login_url="authentication/login/")
+def pages(request):
+    context = {}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]
+        context['segment'] = load_template
+
+        html_template = loader.get_template(load_template)
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+        return render(request, '404.html', context)
+    except:
+        return render(request, '500.html', context)
